82bea7c9aadce22cc8617a43d81837ca82100dc7
gargVader <me.girish.garg@gmail.com>
2024-02-24 02:31:37 +0900

    Optimize imports

--- app/src/main/java/com/example/screensnap/MainActivity.kt
+++ app/src/main/java/com/example/screensnap/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.screensnap.presentation.home.HomeScreen
--- app/src/main/java/com/example/screensnap/ui/theme/Theme.kt
+++ app/src/main/java/com/example/screensnap/ui/theme/Theme.kt
@@ -4,8 +4,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
