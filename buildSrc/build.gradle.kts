6dbec78345fad04c7768239760cef614750a809e
gargVader <me.girish.garg@gmail.com>
2024-03-31 02:38:23 +0900

    buildSrc module added

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+    implementation("com.android.tools.build:gradle:8.1.1")
+}
