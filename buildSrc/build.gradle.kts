af5fc8638002a31354508653549dea740d781282
gargVader <me.girish.garg@gmail.com>
2024-04-02 22:25:58 +0900

    Build running

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,13 +1,3 @@
 plugins {
     `kotlin-dsl`
 }
-
-repositories {
-    google()
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-    implementation("com.android.tools.build:gradle:8.1.1")
-}
