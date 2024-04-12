b41fc7d52b456bb2c95d5fe401fb83018224d6ea
gargVader <me.girish.garg@gmail.com>
2024-04-13 00:23:08 +0900

    SettingsRow updated

--- feature_settings/src/main/java/com/screensnap/feature/settings/SettingsRow.kt
+++ feature_settings/src/main/java/com/screensnap/feature/settings/SettingsRow.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material3.Icon
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,18 +25,25 @@ fun SettingsRow(icon: ImageVector, title: String, currentSetting: String, onClic
         modifier = Modifier
             .fillMaxWidth()
             .clickable(onClick = onClick)
-            .padding(horizontal = 12.dp, vertical = 16.dp),
+            .padding(horizontal = 16.dp, vertical = 16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(icon, contentDescription = "icon", modifier = Modifier.padding(end = 28.dp))
+        Icon(
+            icon,
+            contentDescription = "icon",
+            modifier = Modifier
+                .padding(end = 28.dp)
+                .size(32.dp)
+        )
         Text(
             text = title,
-            style = MaterialTheme.typography.titleMedium,
+            style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.weight(1f)
         )
         Text(
             text = currentSetting,
             style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold,
             textAlign = TextAlign.End,
             color = MaterialTheme.colorScheme.primary
         )
